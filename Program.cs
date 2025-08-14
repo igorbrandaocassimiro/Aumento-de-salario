f3df3bc79143f6c57d22b375df49026cda4d2a90
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-08-14 14:25:19 -0300

    Primeiro commit do programa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] arge)
+    {
+        decimal salarioAtual = 1500m;
+        decimal percentualDeAumento = 10m;
+        decimal novoSalario = salarioAtual + (salarioAtual * percentualDeAumento / 100);
+
+        Console.WriteLine("O novo salário é de R$" + novoSalario.ToString("F2")); 
+
+
+    }
+}
